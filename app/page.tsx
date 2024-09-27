ee424fdaf7f68011a1508996ef7048ce46f9b676
sankhadip-roy <sankhadiproy23@gmail.com>
2024-09-28 00:50:26 +0530

    flipping text added & project's info updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,26 @@
 import { Dock } from "../components/dock";
 import { Cards } from "../components/cards";
 import { BackgroundLines } from "@/components/ui/background-lines";
+import { FlipWords } from "@/components/ui/flip-words";
+import { ExpandableCard } from "@/components/cards2";
+
+
 export default function Home() {
+  const words = ["freelance developer", "fullstack web dev", "cs 4th year undergrad"];
   return (
     <div className="relative min-h-screen">
+      <div className="flex justify-center items-center bg-black">
+        <div className="p-10 text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+          sankhadip roy
+          <FlipWords words={words} />
+        </div>
+      </div>
       <BackgroundLines >
         <div className="z-0 relative">
           <Cards />
         </div>
+        {/* have to fix bugs before showing */}
+        {/* <ExpandableCard /> */}
       </BackgroundLines>
       <Dock />
     </div>
--- components/cards.tsx
+++ components/cards.tsx
@@ -12,37 +12,37 @@ export const projects = [
     {
         title: "chat-app",
         description:
-            "chat room where users can chat with each other in real time",
+            "chat room where users can chat with each other in real time, used socket.io, react, node.js, mongodb",
         link: "https://github.com/sankhadip-roy/chat-app",
     },
     {
         title: "notes",
         description:
-            "notes app for storing personal notes",
+            "notes app for storing personal notes, used next.js, shadcn/ui, typescript, mongodb",
         link: "https://notes-ivory-iota.vercel.app",
     },
     {
         title: "commerce",
         description:
-            "an eBay-like e-commerce auction site that allows users to post auction listings, place bids on listings, comment on those listings, and add listings to a watchlist",
+            "an eBay-like e-commerce auction site, used django, sqlite, docker",
         link: "https://sankha-commerce-serveit.codecult.tech",
     },
     {
         title: "leaf-disease-detection",
         description:
-            "machine learning project to detect leaf disease using image processing",
+            "machine learning project to detect leaf disease using image processing, used next.js, flask, ml, docker",
         link: "https://leaf-orpin.vercel.app",
     },
     {
         title: "cv-application",
         description:
-            " This project is a simple CV application that allows users to input their personal information and display it in a CV format. The project is built using react.",
+            "a simple cv application that allows users to input their personal information and display it in a cv format, used react, tailwind",
         link: "https://github.com/sankhadip-roy/cv-application",
     },
     {
         title: "wiki",
         description:
-            "a wikipedia-like online encyclopedia",
+            "a wikipedia-like online encyclopedia, used django, docker",
         link: "https://wiki-sankha.onrender.com",
     },
 ];
